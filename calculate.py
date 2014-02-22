85b9d05b378431ba6b34dc194a344bc66186577b
Frazer Seymour <frazer@seymourandco.com>
2014-02-22 17:26:08 -0500

    Breakthrough on bus stop location

--- calculate.py
+++ calculate.py
@@ -1,5 +1,14 @@
+from math import sqrt
 from openguelph.classes import *
 from pickle import load
+
+def calculateDistance(x1, y1, x2, y2):
+    x = (x1 - x2)
+    x = x * x
+    y = (y1 - y2)
+    y = y * y
+    return sqrt(x + y)
+
 
 if __name__ == "__main__":
     f = open("./resources/parks.pickle", 'r')
@@ -19,3 +28,20 @@ if __name__ == "__main__":
     
     if user_park != False:
         print user_park.getName(), user_park.getLat(), user_park.getLon()
+
+    distance = 100
+    park_lat = float(user_park.getLat())
+    park_lon = float(user_park.getLon())
+    user_stop = False
+
+    for stop in stops:
+        stop_lat = float(stop.getLat())
+        stop_lon = float(stop.getLon())
+
+        stop_distance = calculateDistance(park_lat, park_lon, stop_lat, stop_lon)
+        if stop_distance < distance:
+            user_stop = stop
+            distance = stop_distance
+
+    if user_stop != False:
+        print user_stop.getName(), user_stop.getLat(), user_stop.getLon()
