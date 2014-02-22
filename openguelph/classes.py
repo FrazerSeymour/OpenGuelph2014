68024c4bf077457bc3a1d31b8f651c1e45c2630e
Frazer Seymour <frazer@seymourandco.com>
2014-02-22 15:24:18 -0500

    Threw Route: command not found and Stop: command not found classes in a Classes: command not found module.

---
+++ openguelph/classes.py
@@ -0,0 +1,29 @@
+class Route:
+    def __init__ (self,  name):
+        self.name = name
+        self.route = []
+        
+    def getName(self): return self.name
+    def getRoute(self): return self.route
+        
+    def appendToRoute(self, item):
+        self.route.append(item)
+
+
+
+class Stop:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+        self.routes = []
+        
+        
+    def getName(self):      return self.name
+    def getLat(self):       return self.lat
+    def getLon(self):       return self.lon
+    def getRoutes(self):    return self.routes
+    
+    def addRoute(self, item):
+        if not self.routes.count(item):
+            self.routes.append(item)
