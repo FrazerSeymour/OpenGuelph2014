f2a94f9f9802b421139daa60d897b833daea622a
Frazer Seymour <frazer@seymourandco.com>
2014-02-23 03:43:15 -0500

    Places pages basically done.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from openguelph.calculations import *
 from pickle import load
 app = Flask(__name__)
 
@@ -9,6 +10,14 @@ def landing():
 @app.route('/go/')
 def go():
     return render_template("go.html", places=places)
+
+@app.route('/place/<placeName>')
+def showPlace(placeName = None):
+    for place in places:
+        if place.getName() == placeName:
+            uc_time = getNextTime("University Centre", place.getUcRoute())
+            dt_time = getNextTime("Guelph Central Station", place.getDtRoute())
+            return render_template("place.html", place=place, dt_time=dt_time, uc_time=uc_time)
 
 if __name__ == '__main__':
     f = open("./places.pickle", 'r')
